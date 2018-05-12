8c1a64cb624d2cf035d080cfbfd4b121eb09aa77
Valentin Degenne <vdegenne@gmail.com>
2018-05-12 17:03:46 +0200

    add StartupConfig to the global scope

--- src/vcms.ts
+++ src/vcms.ts
@@ -4,4 +4,4 @@ export {Router, Routers} from './app';
 export {checkAuthorization} from './app/security';
 export {getConfig} from './config';
 export {CreamModel} from './models/objection-cream';
-export {startServer as start} from './server';
+export {startServer as start, StartupConfig} from './server';
